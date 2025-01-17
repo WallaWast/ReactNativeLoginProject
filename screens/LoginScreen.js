e2ec54350585e4382a8282762a0d98b384a5f763
Wallace Torres <Wallace.DaSilvaTorres@campus-support.ca>
2025-01-17 10:19:22 -0800

    Saving the login token

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,34 @@
+import { useState, useContext } from 'react';
+
 import AuthContent from '../components/Auth/AuthContent';
+import { login } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
-	return <AuthContent isLogin />;
+	const [isAuthenticating, setIsAuthenticating] = useState(false);
+	const authCtx = useContext(AuthContext);
+
+	async function loginHandler({ email, password }) {
+		setIsAuthenticating(true);
+
+		try {
+			const token = await login(email, password);
+			authCtx.authenticate(token);
+		} catch (error) {
+			console.log(error);
+			Alert.alert('Authentication failed!', 'could not log you in, please check your credentials or try again later');
+		}
+
+		setIsAuthenticating(false);
+	}
+
+	if (isAuthenticating) {
+		return <LoadingOverlay message='Logging you in...' />;
+	}
+
+	return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- util/auth.js
+++ util/auth.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
 
-const FIREBASE_URL_V1 = 'https://identitytoolkit.googleapis.com/v1';
-const API_KEY = 'API';
+const FIREBASE_URL_V1 = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+const API_KEY = 'TOKEN';
 
-export async function createUser(email, password) {
-	axios.post(`${FIREBASE_URL_V1}/accounts:signUp?key=${API_KEY}`, {
+async function authenticate(mode, email, password) {
+	const url = `${FIREBASE_URL_V1}${mode}?key=${API_KEY}`;
+
+	const response = await axios.post(url, {
 		email: email,
 		password: password,
 		returnSecureToken: true,
 	});
+
+	return response.data.idToken;
+}
+
+export function createUser(email, password) {
+	return authenticate('signUp', email, password);
+}
+
+export function login(email, password) {
+	return authenticate('signInWithPassword', email, password);
 }
